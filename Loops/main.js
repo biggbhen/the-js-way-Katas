4546b8d7d8c7868642d0877804fbe2abc522fb03
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-03-23 13:02:06 +0100

    the js way

---
+++ Loops/main.js
@@ -0,0 +1,29 @@
+// for (let i = 1; i < 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`${i} is even`);
+//   }else{
+//    console.log(`${i} is odd`)
+//   }
+// }
+
+// for(let i = 50; i <= 100; i++){
+// console.log(`enter number ${i}`);
+// }
+
+// a multiplication table
+// for(let i = 0; i <= 10; i++){
+// console.log(3 * i);
+// }
+
+// for(let i = 1; i <= 100; i++){
+// if(i % 3 === 0 && i % 5 === 0){
+// console.log('fizzbuzz')
+// }else if(i % 3 === 0){
+// console.log('fizz');
+// continue;
+// }else if(i % 5 === 0){
+// console.log('buzz');
+// continue;
+// }
+// console.log(i);
+// }
