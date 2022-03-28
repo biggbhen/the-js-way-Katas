76ccff6cc5baa5ba1e40de1878543a8b8d749cc9
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-03-28 14:41:21 +0100

    finished 92% of the kata

---
+++ Store data in arrays/main.js
@@ -0,0 +1,55 @@
+// Sum of values
+// Write a program that creates the following array, then calculates and shows the sum of its values (42 in that case).
+
+// let sum = 0
+// const values = [3, 11, 7, 2, 9, 10];
+// for(let i=0; i<values.length; i++){
+//  sum+=values[i]
+//  console.log(sum)
+// }
+// values.forEach(value=>{
+//  sum += value
+// console.log(sum);
+// }
+// )
+
+// Musketeers
+// Write a program that:
+
+// Creates an array named musketeers containing values "Athos", "Porthos" and "Aramis".
+// Shows each array element using a for loop.
+// Adds the "D'Artagnan" value to the array.
+// Shows each array element using the forEach() method.
+// Remove poor Aramis.
+// Shows each array element using a for-of loop.
+
+// const musketeers=["Athos", "Porthos", "Aramis"];
+// musketeers.push("D'Artagnan");
+// // musketeers.splice(2, 1);
+// for(let i=0; i < musketeers.length; i++){
+// console.log(musketeers[i])
+// }
+// musketeers.forEach(musketeer => {
+//  console.log(musketeer)
+// })
+// for(const i of musketeers){
+// console.log(i)
+// }
+
+
+// Array maximum
+// Write a program that creates the following array, then calculates and shows the array's maximum value.
+// const values = [3, 11, 7, 2, 9, 10];
+
+//  console.log(Math.max(3, 11, 7, 2, 9, 10))
+
+
+// List of words
+// Write a program that asks the user for a word until the user types "stop". The program then shows each of these words, except "stop".
+
+// let words = []
+// let input= 'we are here again'
+// console.log(words.push(input));
+ 
+
+
